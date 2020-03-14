c533f2bdcaa91126c3626e0781d0f0c32687b234
Jakub Sadowy <sadowy97@gmail.com>
2020-03-14 12:05:44 +0100

    Add Editor class to ViewModel
    Editor class in viewmodel keeps an instance of model's Text class and provide methods for combining paragraphs from model to one string.

---
+++ noter/WpfApp1/ViewModel/Editor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noter.ViewModel
+{
+    using Model;
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class Editor : INotifyPropertyChanged
+    {
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged([CallerMemberName]string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+        private Text text = new Text();
+
+        public string Text
+        {
+            get
+            {
+                StringBuilder result = new StringBuilder();
+                foreach (string paragraph in text.Paragraphs)
+                    result.Append(paragraph);
+                return result.ToString();
+            }
+            set
+            {
+                text.Paragraphs = value.Split('\n');
+                onPropertyChanged();
+            }
+        }
+
+    }
+}
